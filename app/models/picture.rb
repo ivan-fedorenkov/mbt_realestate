fcc6bd389cda7742eee1aaa6c1c46ae965c5a156
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2013-01-08 14:54:31 +0400

    Changed dropbox application type

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -5,15 +5,15 @@ class Picture < ActiveRecord::Base
       :styles => { :thumb => ["116x90>", :png], :big => ["620x410>", :png] },
       :storage => :dropbox, 
       :dropbox_credentials => {
-        :app_key => "wddwaipvyvznn1x", 
-        :app_secret => "t4qb91chxn8mv76", 
-        :access_token => "zsa8k7j4xv6ebbw", 
-        :access_token_secret => "9ldfui06gtldh9o", 
-        :access_type => "app_folder", 
+        :app_key => "pxeb46rajwqc67y", 
+        :app_secret => "v5ird3hbrk3kv75", 
+        :access_token => "71jg9eo2l2x3q9v", 
+        :access_token_secret => "70d9w22lc5w63ss", 
+        :access_type => "dropbox", 
         :user_id => "52135757" 
       },
       :dropbox_options => {
-        :path => proc { |style| "residential_pictures/#{style}/#{id}_#{picture.original_filename}" }
+        :path => proc { |style| "Apps/MBT/pictures/#{style}/#{id}_#{picture.original_filename}" }
       }
       
   belongs_to :imageable, :polymorphic => true
