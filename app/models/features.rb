a492325df6d51d3bb87a6327d3cef4519abdc4ec
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2013-02-02 20:46:08 +0400

    Fixed a bug when feature specifications are saved in incorrect state and then we cannot load the residential

--- app/models/features.rb
+++ app/models/features.rb
@@ -1,20 +1,20 @@
 class Features
   
-  @@option_regexp = %r{\s*[^:@;]+}
-  @@feature_regexp = %r{\s*[^:@;]+:(#{@@option_regexp}@@)*#{@@option_regexp}}
-  @@features_regexp = %r{^(#{@@feature_regexp};)*#{@@feature_regexp}\s*$}
+  @@option_regexp = %r{\s*[^@]+}
+  @@feature_regexp = %r{\s*[^@]+=>(#{@@option_regexp}@@)*#{@@option_regexp}}
+  @@features_regexp = %r{^(#{@@feature_regexp}@@@)*#{@@feature_regexp}\s*$}
   
   def initialize(features_string)
     @features = {}
     if ((features_string) and (!features_string.blank?))
       if features_string.match(@@features_regexp)
-        features = features_string.split(%r{\s*;\s*})
+        features = features_string.split(%r{\s*@@@\s*})
         features.each do |feature|
-          feature_name, feature_elements = feature.split(%r{\s*:\s*})
+          feature_name, feature_elements = feature.split(%r{\s*=>\s*})
           @features[feature_name] = feature_elements.split(%r{\s*@@\s*})
         end
       else
-        raise "Features string must be of the following pattern: #{@@features_regexp}"
+        @features["INVALID"] = features_string
       end
     end
   end
