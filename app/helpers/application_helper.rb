d802263fab412c25d886f8b02717172dcefe52c5
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2013-02-24 12:30:01 +0400

    fixed a bug inside truncate_words helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -64,7 +64,9 @@ module ApplicationHelper
   end
   
   def truncate_words(text, length = 300, end_string = ' …')
-    words = text.split()
-    words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+    if text
+      words = text.split()
+      words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
+    end
   end
 end
