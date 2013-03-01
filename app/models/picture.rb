d1e29dc4490261ad10c262118143855426f8757a
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2013-03-01 18:41:15 +0400

    changed path inside picture model

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -3,7 +3,7 @@ class Picture < ActiveRecord::Base
   attr_accessible :picture
   has_attached_file :picture,
       :styles => { :thumb => ["116x90>", :jpg], :big => ["620x410>", :jpg] },
-      :path => "MBT/pictures/:style/:id_:original_filename"
+      :path => "MBT/pictures/:style/:id_:filename"
 
       
   belongs_to :imageable, :polymorphic => true
