8c6552cbc3b0a9f80e1b0cc3d6ce0eb88434655d
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2013-03-14 18:10:35 +0400

    Yandex metrics

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -69,4 +69,42 @@ module ApplicationHelper
       words[0..(length-1)].join(' ') + (words.length > length ? end_string : '')
     end
   end
+
+  def render_yandex_metric
+    code = %Q{
+      <!-- Yandex.Metrika counter -->
+      <script type="text/javascript">
+      (function (d, w, c) {
+          (w[c] = w[c] || []).push(function() {
+              try {
+                  w.yaCounter20575636 = new Ya.Metrika({id:20575636,
+                          webvisor:true,
+                          clickmap:true,
+                          trackLinks:true,
+                          accurateTrackBounce:true});
+              } catch(e) { }
+          });
+
+          var n = d.getElementsByTagName("script")[0],
+              s = d.createElement("script"),
+              f = function () { n.parentNode.insertBefore(s, n); };
+          s.type = "text/javascript";
+          s.async = true;
+          s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+          if (w.opera == "[object Opera]") {
+              d.addEventListener("DOMContentLoaded", f, false);
+          } else { f(); }
+      })(document, window, "yandex_metrika_callbacks");
+      </script>
+      <noscript><div><img src="//mc.yandex.ru/watch/20575636" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+      <!-- /Yandex.Metrika counter -->
+    }
+
+    if Rails.env.production?
+      return code
+    else
+      return ""
+    end
+  end
 end
